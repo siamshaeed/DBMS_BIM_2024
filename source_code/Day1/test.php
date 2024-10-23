63d2a8b3be2c7215d541c140bb4111594b84c5b2
siamshaeed <siamshaeed@gmail.com>
2024-10-23 11:17:38 +0600

    php syntex

---
+++ source_code/Day1/test.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Embedded in HTML</title>
+</head>
+<body>
+    <h1><?php echo "Welcome to my website" ?></h1>
+    <p><?php echo "Hellow Bangladesh" ?></p>
+</body>
+</html>
