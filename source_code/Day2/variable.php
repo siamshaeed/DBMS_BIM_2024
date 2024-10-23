1496e702aaac557be7cdbcdd30c04f0a07c38f3a
siamshaeed <siamshaeed@gmail.com>
2024-10-23 12:10:55 +0600

    php variable

---
+++ source_code/Day2/variable.php
@@ -0,0 +1,28 @@
+<?php
+// Declaring variables
+$name = "John";             // String
+$age = 25;                 // Integer
+$height = 5.9;            // Float
+$isStudent = true;       // Boolean
+
+// Outputting the values of variables
+echo "Name: " . $name . "<br>";           // Output: Name: John
+echo "Age: " . $age . "<br>";            // Output: Age: 25
+echo "Height: " . $height . "<br>";     // Output: Height: 5.9
+echo "Is student: " . $isStudent . "<br>";      // Output: Is student: 1 (true is displayed as 1)
+
+// Example
+$name = "Ridoy";
+$age = 30;
+
+echo "My name is $name and I am $age years old." . "<br>";  // Output: My name is Ridoy and I am 30 years old.
+
+// How to write standard variable names in PHP (Standard variable code more readable, maintainable, and professional.)
+$name = "Ridoy";
+$firstName = "Ridoy";     // CamelCase
+$first_name = "Ridoy";   // Snake_Case
+
+// Case Sensitivity
+$Name = "Ridoy";
+$name = "Rofique";  // Different variable from $userName
+?>
